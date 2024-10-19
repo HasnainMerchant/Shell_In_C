2a7538025c4d3ff4facda206a960d48f9fb60cc7
Hasnain Merchant <hasnainmerchant0@gmail.com>
2024-10-19 14:41:20 +0530

    Final commit of code with code optimizations and bugs solving

--- shc.c
+++ shc.c
@@ -1,11 +1,98 @@
+/***************************************************************************//**
+
+  @file         main.c
+
+  @author       Hasnain Merchant
+
+  @date         Saturday,  19 October 2024
+
+  @brief        SCH (Shell in C)
+
+*******************************************************************************/
+#include <sys/wait.h>
+#include <sys/types.h>
 #include <unistd.h> //chdir(), fork(), exec(), pid_t
 #include <stdlib.h> //malloc(), realloc(), free(), exit(), execvp(), EXIT_SUCCESS, EXIT_FAILURE
 #include <stdio.h> //fprintf(), printf(), stderr, getchar(), perror()
 #include <string.h> //strcmp(), strtok()
 
+//Creating Shell Builtins (cd, help, exit)
+/* Function Decalrations for builtin shell Commands*/
+int sch_cd(char **args);
+int sch_help(char **args);
+int sch_exit(char **args);
+
+/*List of builtin commands, followed by corresponding functions*/
+
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit"
+};
+
+/*Array of function pointers
+Each function takes a char ** (pointer to a pointer to a char) as an argument and returns an int
+The array is initialized with the address of the function e.g sch_cd. This means sch_cd is a function that matches 
+the signature int function_name(char **)
+*/
+int (*builtin_func[]) (char **) = {
+    &sch_cd,
+    &sch_help,
+    &sch_exit
+};
+
+int sch_num_builtins() {
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+//Function Implementations
+int sch_cd(char **args) {
+    if(args[1] == NULL) {
+        fprintf(stderr, "sch: expected argument to \"cd\"\n");
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("SCH");
+        }
+    }
+    return 1;
+}
+
+int sch_help(char **args) {
+    int i;
+    printf("Hasnain Merchant's SCH\n");
+    printf("Type program names and arguments, and hit enter.\n");
+    printf("The following are built in:\n");
+
+    for(i=0;i<sch_num_builtins();i++) {
+        printf("   %s\n", builtin_str[i]);
+    }
+
+    printf("Use the man command for information on other programs.\n");
+    return 1;
+}
+
+int sch_exit(char **args) {
+    return 0;
+}
+
+//Putting togther builtins and processes
+int sch_execute(char **args) {
+    int i;
+    if(args[0] == NULL) {
+        return 1;
+    }
+
+    for(i = 0;i<sch_num_builtins();i++) {
+        if(strcmp(args[0], builtin_str[i] == 0)) {
+            return (*builtin_func[i])(args);
+        }
+    }
+    return sch_launch(args);
+}
+
 //Starting Shell Processes
 int sch_launch(char **args) {
-    pid_t pid, wpid;
+    pid_t pid;
     int status;
 
     pid = fork();
@@ -21,7 +108,7 @@ int sch_launch(char **args) {
     } else {
         //Parent Process
         do {
-            wpid = waitpid(pid, &status, WUNTRACED);
+            waitpid(pid, &status, WUNTRACED);
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
     return 1;
@@ -33,7 +120,7 @@ int sch_launch(char **args) {
 char **shc_split_line(char *line){
     int buffer_size = SHC_TOK_BUFSIZE, pos = 0;
     char **tokens = malloc(buffer_size * sizeof(char*));
-    char *token;
+    char *token, **tokens_backup;
 
     if(!tokens) {
         fprintf(stderr, "lsh: allocation error\n");
@@ -46,8 +133,10 @@ char **shc_split_line(char *line){
         pos++;
         if(pos >= buffer_size) {
             buffer_size += SHC_TOK_BUFSIZE;
+            tokens_backup = tokens;
             tokens = realloc(tokens, buffer_size * sizeof(char*));
             if(!tokens) {
+                free(tokens_backup);
                 fprintf(stderr, "lsh: allocation error\n");
                 exit(EXIT_FAILURE);
             }
@@ -67,7 +156,7 @@ char *sch_read_line(void) {
         if(feof(stdin)) {
             exit(EXIT_SUCCESS); //received and EOF
         } else {
-            perror("readline");
+            perror("sch: getline\n");
             exit(EXIT_FAILURE);
         }
     }
@@ -92,12 +181,15 @@ char *sch_read_line_old(void) {
         c = getchar();
 
         //If we hit EOF, replcae it with a null character and return.
-        if(c == EOF || c == '\n'){
+        if(c == EOF){
+            exit(EXIT_SUCCESS);
+        } else if (c == '\n') {
             buffer[pos] = '\0';
             return buffer;
         } else {
-            buffer[pos++] = c;
+            buffer[pos] = c;
         }
+        pos++;
 
         //If we have exceeded the buffer, reallocate
         if(pos >= buffer_size) {
@@ -118,7 +210,7 @@ void sch_loop(void) {
     int status;
 
     do {
-        printf("$ ");
+        printf(">> ");
         line = sch_read_line();
         args = sch_split_line(line);
         status = sch_execute(args);
